afd129882fa0a20e4c2ee30139d2d9861f5aeb10
hednailiAutomation <hednailiAutomation@gmail.com>
2024-10-01 20:57:08 +0100

    update change password steps

--- BDDProject/src/test/java/stepDefinitions/ChangePasswordSteps.java
+++ BDDProject/src/test/java/stepDefinitions/ChangePasswordSteps.java
@@ -1,15 +1,11 @@
 package stepDefinitions;
 
-import java.util.List;
-import java.util.Map;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import commun.BasePage;
 import commun.Hooks;
-import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
